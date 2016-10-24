ae837db68b16b2a74ccba1b95be556cbca3bad8d
sgskinner <scott@skinner.io>
2016-10-23 19:06:35 -0700

    Adding spring deps, added per file GPL.

--- src/main/java/org/sgs/atbot/service/ArchiveIsService.java
+++ src/main/java/org/sgs/atbot/service/ArchiveIsService.java
@@ -19,41 +19,10 @@
  * Copyright (C) 2016  S.G. Skinner
  */
 
-package org.sgs.archivethisbot;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+package org.sgs.atbot.service;
 
 /**
- * Unit test for simple App.
+ * Created by u00 on 10/23/16.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public interface ArchiveIsService {
 }
--- src/main/java/org/sgs/atbot/service/impl/ArchiveIsServiceImpl.java
+++ src/main/java/org/sgs/atbot/service/impl/ArchiveIsServiceImpl.java
@@ -19,41 +19,10 @@
  * Copyright (C) 2016  S.G. Skinner
  */
 
-package org.sgs.archivethisbot;
+package org.sgs.atbot.service.impl;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.sgs.atbot.service.ArchiveIsService;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class ArchiveIsServiceImpl implements ArchiveIsService {
 }
